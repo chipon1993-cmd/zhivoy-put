83468b58c10cdbc1a5b6d6f24f50d6069afaff3f
Andrii Chepelovskyi <chipon1993@email.com>
2026-05-24 21:36:44 +0200

    add PWA support, favicon, icons, service worker, update domain to atlas-life

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'zhivoy-put-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/variables.css',
+  '/css/base.css',
+  '/css/layout.css',
+  '/css/components.css',
+  '/css/header.css',
+  '/css/footer.css',
+  '/css/responsive.css',
+  '/js/components.js',
+  '/js/cms-loader.js',
+  '/favicon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
